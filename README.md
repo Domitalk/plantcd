f8faac64c89ba43c47702618aa1ac97eceec3940
Dominic Chu <dominic.m.chu@gmail.com>
2021-06-17 12:31:18 -0700

    typos and next steps

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Next steps 
 
-1. redux/thunk
+<!-- 1. redux/thunk -->
 2. some more specific tests 
 3. login modal 
 4. individual pages 
--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -1,9 +1,8 @@
+import BackendAddress from '../../constants/BackendAddress'
+
 export const SET_USER = 'SET_USER';
 export const CLEAR_NAME = 'CLEAR_NAME';
 export const TOGGLE_LOG = 'TOGGLE_LOG';
-
-//
-import BackendAddress from '../../constants/BackendAddress'
 
 export const createUser = (userInfo) => {
     return async dispatch => {
@@ -16,7 +15,7 @@ export const createUser = (userInfo) => {
                 ... userInfo
             })
         })
-        const rData = await reponse.json()
+        const rData = await response.json()
 
         // figure out how to payload the rData and change the reducer function that corresponds 
         dispatch({ 
