5d31d5a4aa6a31b673de7c23625b3ae76fde1ed9
Dominic Chu <dominic.m.chu@gmail.com>
2021-06-17 11:19:53 -0700

    fixed redux issue with tests

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,8 +1,8 @@
-import { useSelector , connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 const HomeScreen = () => {
     // make a dummy testing for user_name 
-    const user_name = useSelector(state => state.user_name)
+    const { user_name } = useSelector(state => state.user_name)
 
     return (
         <div style={{ height: "500px", width: "500px" }}>HomeScreen
--- src/components/HomeScreen.test.js
+++ src/components/HomeScreen.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import HomeScreen from './HomeScreen';
+import { Provider } from 'react-redux';
+import store from '../store/index.js'
 
 test('description', () => {
-    render(<HomeScreen />)
+    render(<Provider store={store}><HomeScreen /></Provider>)
     const homeScreenPlaceholder = screen.getByText(/homescreen/i)
     expect(homeScreenPlaceholder).toBeInTheDocument()
 })
